9ffc7ed537e8e4c0ed2dd378089ad45039e2ca8d
Drogon-dev <okenedaniel44@gmail.com>
2022-06-30 17:45:34 -0700

    added file 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - prints out programming is like  building a multilingual puzzle to stdout
+ * Return: 0 if executed properly or non-zero otherwise
+ */
+
+int main() {
+	
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
+}
