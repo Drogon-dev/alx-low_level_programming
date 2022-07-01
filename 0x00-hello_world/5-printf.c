c904422b44eca9278075e01221b62c79c08dc9a3
Drogon-dev <okenedaniel44@gmail.com>
2022-06-30 18:03:31 -0700

    added file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
-
+/**
+ * main - prints with proper grammar, but the outcome is apiece of art
+ * return: 0 if executed properly, non-zero if otherwise
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
