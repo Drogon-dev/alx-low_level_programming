6328ce1abb3854ceec18519b057c9f064a9be56e
Drogon-dev <okenedaniel44@gmail.com>
2022-07-01 20:53:32 -0700

    added file 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.>
+/**
+ * main - prints all alphabelts in lowercase
+ * Return: 0 if executed properly, non-zero if otherwise
+ */
+int main(void)
+{
+	char ch;
+	
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
