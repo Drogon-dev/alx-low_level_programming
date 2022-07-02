6dd52e2f032920de6fbdefa6f4ddd1d9818fb28f
Drogon-dev <69829036+Drogon-dev@users.noreply.github.com>
2022-07-02 05:47:22 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,19 +1,18 @@
 #include <stdio.h>
 
 /**
- * main - prints all single digie=t numbers of base 10 starting
- * from 0, followed by a newline, using putchar
- * Return: always 0 (success)
+ * main - prints all single digit numbers of base 10 starting from 0,
+ * followed by a new line, using putchar
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int n;
 
-
 	for (n = 48; n < 58; n++)
 	{
 		putchar(n);
 	}
-	putchar("\n");
+	putchar('\n');
 	return (0);
 }
