105d72ed31168d4e1e60666972a36b73294dae47
Drogon-dev <okenedaniel44@gmail.com>
2022-07-05 16:13:31 -0700

    updated main.h and added 1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the english alphabet in
+ * lowercase followed by a new line
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+	return;
+}
