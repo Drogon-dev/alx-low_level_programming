b78ffd67d26449ce7bf3a49829ca7a25c5af30ee
Drogon-dev <okenedaniel44@gmail.com>
2022-07-05 17:10:07 -0700

    updated main.h and added 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * _isaplha - Checks if a character is alphabetic
+ * _isalpha - Checks if a character is alphabetic
  * @c : character to be checked
  * Return: 1 if true, 0 if otherwise
  */
 
-int _isaplha(int c)
+int _isalpha(int c)
 {
 	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
