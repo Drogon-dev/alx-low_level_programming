372984f985a96828120d3460de22a1946b65c923
Drogon-dev <okenedaniel44@gmail.com>
2022-07-05 17:39:03 -0700

    updated main.h and added 6.abs.c

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _abs - prints the absolute value of a character
+ * @c : character to be checked
+ * Return: the absolute value of the charactet
+ */
+
+int _abs(int c)
+{
+	if (c > 0)
+	{
+		return (c);
+	}
+	else if (c > 0)
+	{
+		return (-c):
+	}
+	else
+	{
+		return (0);
+	}
+}
