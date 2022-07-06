c4eaa0d06fd70f1167c8f3442cbbda34e19da41a
Drogon-dev <okenedaniel44@gmail.com>
2022-07-05 19:55:24 -0700

    updated main.h and added 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -18,7 +18,7 @@ void times_table(void)
 			{
 				_putchar(k + '0');
 			}
-			if ( k < 10 && j != 0)
+			if (k < 10 && j != 0)
 			{
 				_putchar(',');
 				_putchar(' ');
@@ -29,7 +29,7 @@ void times_table(void)
 			{
 				_putchar(',');
 				_putchar(' ');
-				_putchar((k / '0') + '0');
+				_putchar((k / 10) + '0');
 				_putchar((k % 10) + '0');
 			}
 			
