d81504a301273cd7f637531047cdf7df5de67a75
Drogon-dev <okenedaniel44@gmail.com>
2022-07-07 05:21:57 -0700

    added file 1-main.c

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - causes an infinte loop
+ * Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	printf("Infinte loop imcoming :(\n");
+
+	i = 0;
+
+	/* while (i < 10)
+	{
+		putchar(i);
+	} */
+
+	printf("Infinte loop avoided! \\0/\n");
+	return (0);
+}
