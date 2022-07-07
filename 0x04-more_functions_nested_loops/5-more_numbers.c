4e0adde2bf2a178b6b1ff1a053999f7daacc38b9
Drogon-dev <okenedaniel44@gmail.com>
2022-07-07 16:23:28 -0700

    added file 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,9 +13,12 @@ void more_numbers(void)
 	{
 		for (i = 0; i < 15; i++)
 		{
-			_putchar(i + '0');
+			if (i >= 10)
+			{
+				_putchar(i / 10 + '0');
+			}
+			_putchar(i % 10 + '0');
 		}
-		_putchar('\n');
 		j++;
 	}
 	_putchar('\n');
