25ac276b8dfc1554d13d9e1e75ca54c57b4a399e
Drogon-dev <69829036+Drogon-dev@users.noreply.github.com>
2022-07-08 01:05:00 +0100

    Update 7-print_daigonal.c

--- 0x04-more_functions_nested_loops/7-print_daigonal.c
+++ 0x04-more_functions_nested_loops/7-print_daigonal.c
@@ -1,13 +1,16 @@
 #include "main.h"
 
 /**
- * print_diagonal - draws a diagonal line
- * @n: integer n
+ * print_diagonal - draws a diagonal line on the terminal
+ * @n: number of times the character \ should be printed
  */
 
 void print_diagonal(int n)
 {
-	if (n > 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
 	{
 		int i, j;
 
@@ -16,15 +19,11 @@ void print_diagonal(int n)
 			for (j = 0; j < n; j++)
 			{
 				if (j == i)
-				{
-					_putcahr('\\');
-				}
+					_putchar('\\');
 				else if (j < i)
-				{
 					_putchar(' ');
-				}
 			}
-			_putchar('\n')
+			_putchar('\n');
 		}
 	}
 }
