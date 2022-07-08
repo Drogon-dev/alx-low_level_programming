e3fd46114b307e152f438f6d48e4430edab205d8
Drogon-dev <okenedaniel44@gmail.com>
2022-07-07 18:23:54 -0700

    added file 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a traingle with the # sign
+ * @size - lenght of traingle
+ */
+
+void print_traingle(int size)
+{
+	if (size > 0)
+	{
+		int i, j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size; j > 0; j--)
+			{
+				if (j > i)
+				{
+					_putchar(' ');
+				}
+				else if ( j <= i)
+				{
+					_putchar('#');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
