8f3fe39290333e9c57736d9b62ee14dfc8695c48
Drogon-dev <okenedaniel44@gmail.com>
2022-07-12 21:16:41 -0700

    updated file 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -17,16 +17,16 @@ void puts_half(char *str)
 	}
 	if (len % 2 == 0)
 	{
-		for (i = (len / 2); i < len; i++)
+		for (i = (len / 2); str[i] != '\0'; i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = (len - 1 / 2); i < len; i++)
+		for (i = (len - 1) / 2; i < len - 1; i++)
 		{
-			_putchar(str[i]);
+			_putchar(str[i + 1]);
 		}
 	}
 	_putchar('\n');
