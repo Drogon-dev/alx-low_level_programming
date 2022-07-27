65e05807a4ea6b2953e7d7c349cec88648f9e1f7
Drogon-dev <okenedaniel44@gmail.com>
2022-07-26 17:16:49 -0700

    created a new directory and added file 0 to 4 and file 100 and 101

---
+++ 0x0B-malloc_free/_putchar.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c ot stdout
+ * @c: The character to print
+ *
+ * Return: On success 1
+ * On error, -1 is returne, and errno is set appropriately
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
