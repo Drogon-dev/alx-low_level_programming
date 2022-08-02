d03c39909cc788dcd55cba360fb3f062546ee4b8
Drogon-dev <okenedaniel44@gmail.com>
2022-08-01 21:08:57 -0700

    hot fix with file 2

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,6 +16,8 @@ void print_dog(struct dog *d)
 		d->name = "(nil)";
 	if (d->owner == NULL)
 		d->owner = "(nil)";
+	if (d->age == NULL)
+		d->age = "(nil)";
 
 	printf("Name: %s\nAge: %fOwner: %s\n", d->name, d->age, d->owner);
 }
