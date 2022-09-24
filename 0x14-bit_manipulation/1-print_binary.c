58e44bdb75a30482fb0b769733d177080dacc792
Drogon-dev <okenedaniel44@gmail.com>
2022-09-23 22:00:52 -0700

    fixed errors with files

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,16 +6,15 @@
  */
 void print_binary(unsigned long int n)
 {
-    char binary;
+	char binary;
 
-    if (n == 0)
-        _putchar('0');
-    
-    if(n > 0)
-    {
-        binary = ((n & 1) == 0 ? '0' : '1');
-        if (n >> 1 != 0)
-            print_binary(n >> 1);
-        _putchar(binary);
-    }
+	if (n == 0)
+		_putchar('0');
+	if (n > 0)
+	{
+		binary = ((n & 1) == 0 ? '0' : '1');
+		if (n >> 1 != 0)
+			print_binary(n >> 1);
+		_putchar(binary);
+	}
 }
--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -4,17 +4,15 @@
  * get_bit - returns the value of a bit at a given index
  * @n: number to convert to binary
  * @index: index to find
- * 
+ *
  * Return: value of index searched
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    int index_value;
+	int index_value;
 
-    if (index > 63)
-        return(-1);
-
-    index_value = (n >> index) & 1;
-    
-    return(index_value);
+	if (index > 63)
+		return (-1);
+	index_value = (n >> index) & 1;
+	return (index_value);
 }
